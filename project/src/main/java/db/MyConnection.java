084e8319747f1582d63198d2304c401bafbf709b
Nikhil Jadhav <nikhiljadhav5009@gmail.com>
2024-07-30 20:01:46 +0530

    Connection between java and mysql

---
+++ project/src/main/java/db/MyConnection.java
@@ -0,0 +1,31 @@
+package db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    public static Connection connection;
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hiderdb?useSSL=false", "root", "Nikhil@1234");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Connection established successfully");
+        return connection;
+    }
+    public static void closeConnection() {
+        if(connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyConnection.getConnection();
+    }
+}
